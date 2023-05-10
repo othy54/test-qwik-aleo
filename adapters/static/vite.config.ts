ce861f0dd6c02d0dbdc491501c42b12f46d5ad1c
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-10 17:23:12 +0200

    update

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: 'https://yoursite.qwik.dev',
+        origin: 'https://test-qwik-aleo.netlify.app',
       }),
     ],
   };
