ecfcf0a6f044cbf3b656319db12f571c78ea6f74
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-11 17:31:54 +0200

    update

---
+++ src/components/lotties/lotties.module.tsx
@@ -0,0 +1,163 @@
+import {
+  component$,
+  useStylesScoped$,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import styles from "./lotties.css?inline";
+import lottie from "lottie-web";
+import { animate } from "motion";
+
+interface List {
+  title?: string;
+  content?: string;
+}
+
+interface ItemProps {
+  listLotties?: string[];
+  invert?: boolean;
+  label?: string;
+  title?: string;
+  list?: Array<List>;
+}
+
+export default component$<ItemProps>((props) => {
+  useStylesScoped$(styles);
+  useVisibleTask$(() => {
+    document
+      .querySelector("." + props.label + "-item-0")
+      ?.classList.add("item--active");
+    const animation = lottie.loadAnimation({
+      container: document.querySelector("." + props.label + "")!,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      // path: `/lotties/${props.listLotties[step.value]}.json`
+      path: "/lotties/ameliore-image.json",
+    });
+
+    const anim = () => {
+      const animation = animate(
+        "." + props.label + "-item-" + step.value + " .spin-progress",
+        { strokeDashoffset: 0 },
+        { duration: 5, easing: "linear" }
+      );
+      animation.finished.then(() => {
+        if (
+          document.querySelector<HTMLElement>(
+            "." + props.label + "-item-" + step.value + " .spin-progress"
+          )!.style.strokeDashoffset === "0"
+        ) {
+          document.querySelector<HTMLElement>(
+            "." + props.label + "-item-" + step.value + " .spin-progress"
+          )!.style.strokeDashoffset = "66px";
+          document
+            .querySelector<HTMLElement>(
+              "." + props.label + "-item-" + step.value
+            )!
+            .classList.remove("item--active");
+          step.value++;
+          if (step.value > 3) {
+            step.value = 0;
+          }
+          document
+            .querySelector<HTMLElement>(
+              "." + props.label + "-item-" + step.value
+            )!
+            .classList.add("item--active");
+          reloadAnimation();
+          anim();
+        }
+      });
+    };
+  });
+
+  //   const state = useStore({
+  //     animationLottie: $(() => {
+  //       lottie.loadAnimation({
+  //         container: "",
+  //       });
+  //     }),
+  //   });
+
+  return (
+    <>
+      <div class="grid grid-cols-12">
+        <div
+          class={[
+            "rounded-3xl p-20 column-lottie aspect-square order-2 lg:order-1 mt-8 lg:mt-0",
+            props.invert
+              ? "lg:order-2 col-span-12 lg:col-span-5 lg:col-start-7"
+              : "lg:order-1 col-span-12 lg:col-span-4 lg:col-start-2",
+          ]}
+        >
+          <div class={props.label}></div>
+        </div>
+        <div
+          class={[
+            "flex items-center order-1",
+            props.invert
+              ? "lg:order-1 col-span-12 lg:col-span-4 lg:col-start-2"
+              : "col-span-12 lg:col-span-5 lg:col-start-7",
+          ]}
+        >
+          <div>
+            <h2 class="italic font-black text-[32px] lg:text-[40px] text-accent-300">
+              {props.title}
+            </h2>
+            <ul class="mt-6 flex flex-col gap-y-6">
+              {props.list!.map((item, i) => (
+                <>
+                  <li
+                    key={"visibility-" + i}
+                    class={[
+                      "flex gap-x-4 item transition-all duration-300",
+                      props.label + "-item-" + i + " " + props.label + "-item",
+                    ]}
+                  >
+                    <svg
+                      class="flex-shrink-0"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="12"
+                        cy="12"
+                        r="10.5"
+                        transform="rotate(-90 12 12)"
+                        stroke="#280837"
+                        stroke-width="3"
+                      ></circle>
+                      <circle
+                        cx="12"
+                        cy="12"
+                        r="10.5"
+                        transform="rotate(-90 12 12)"
+                        stroke="#A2CCEE"
+                        stroke-width="3"
+                        class="spin-progress"
+                        style="stroke-dashoffset: 66px; stroke-dasharray: 66px;"
+                      ></circle>
+                    </svg>
+                    <div class="min-h-0">
+                      <h3 class="italic font-black text-xl text-accent-500">
+                        {item.title}
+                      </h3>
+                      <div class="wrapper-content">
+                        <p class="mt-2 font-medium text-accent-500 overflow-hidden min-h-0">
+                          {item.content}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,109 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Image } from "@unpic/qwik";
-import styles from "./index-scoped-style.css?inline";
+import styles from "./home.css?inline";
 
 import Header from "~/components/starter/header/header";
+import Lotties from "~/components/lotties/lotties.module";
 // import Footer from '~/components/starter/footer/footer';
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const state = useStore({
+    newspapers: [
+      {
+        link: "https://www.lefigaro.fr/services/aleo-l-agence-web-n01-dans-la-creation-de-sites-internet-pour-les-tpe-pme-20210716",
+        src: "/img/logo-figaro.svg",
+        alt: "logo figaro",
+      },
+      {
+        link: "https://www.bfmtv.com/economie/professionnels/aleo-l-agence-de-communication-pour-tpe-pme-qui-va-propulser-votre-activite_AN-202107290333.html",
+        src: "/img/logo-bfm.svg",
+        alt: "logo bfm",
+      },
+      {
+        link: "https://www.challenges.fr/entreprise/aleo-l-agence-de-communication-100-dediee-aux-tpe-pme_773971",
+        src: "/img/logo-challenges.svg",
+        alt: "logo challenges",
+      },
+      {
+        link: "https://www.latribune.fr/supplement/comment-l-agence-web-aleo-souhaite-booster-la-presence-en-ligne-des-tpe-pme-890514.html",
+        src: "/img/logo-la-tribune.svg",
+        alt: "logo la tribune",
+      },
+      {
+        link: "https://www.forbes.fr/brandvoice/10000-entreprises-conquises-pour-aleo-lagence-web-n1-des-tpe-pme/",
+        src: "/img/logo-forbes.svg",
+        alt: "logo forbes",
+      },
+    ],
+
+    activities: [
+      {
+        link: "/site-internet",
+        emoji: "/img/emojis/emoji-laptop.png",
+        label: "Créer ton site web",
+        alt: "emoji laptop",
+      },
+      {
+        link: "/",
+        emoji: "/img/emojis/emoji-megaphone.png",
+        label: "Conquérir plus de clients",
+        alt: "emoji megaphone",
+      },
+      {
+        link: "/",
+        emoji: "/img/emojis/emoji-hearts.png",
+        label: "Développer tes réseaux sociaux",
+        alt: "emoji hearts",
+      },
+      {
+        link: "/",
+        emoji: "/img/emojis/emoji-pen.png",
+        label: "Améliorer ton image",
+        alt: "emoji pen",
+      },
+      {
+        link: "/",
+        emoji: "/img/emojis/emoji-chat.png",
+        label: "Échanger gratuitement avec nos experts",
+        alt: "emoji chat",
+      },
+    ],
+
+    lottieVisibility: {
+      listLotties: [
+        "ameliore-image",
+        "multiplie-contacts",
+        "notoriete",
+        "fidelisation",
+      ],
+      list: [
+        {
+          title: "Améliore ton image",
+          content:
+            "Améliorer la manière dont tes clients te perçoivent te permettra de sortir du lot et de dépasser la concurrence.",
+        },
+        {
+          title: "Multiplie tes contacts",
+          content:
+            "Les demandes de devis sont le moteur du développement commercial de ton entreprise. Avec ALEO, génère un maximum d’opportunités.",
+        },
+        {
+          title: "Augmente ta notoriété",
+          content:
+            "Une bonne notoriété est indispensable pour ton business. Alors sois présent et actif pour marquer les esprits de tes futurs clients !",
+        },
+        {
+          title: "Fidélise tes clients",
+          content:
+            "Loin des yeux, loin du cœur… Noue une relation durable et de confiance avec ta clientèle grâce à une communication régulière et des actions ciblées.",
+        },
+      ],
+      title: "Propulse ta visibilité et atteins tes objectifs",
+    },
+  });
+
   return (
     <>
       <section class="section section-hero w-full bg-primary-500">
@@ -54,11 +150,179 @@ export default component$(() => {
                   class="absolute -bottom-32"
                   priority={true}
                   breakpoints={[350, 700, 745, 1490]}
-                  height={720}
-                  width={669}
+                  height={669}
+                  width={720}
                   sizes="(max-width:768px) 350px, (max-width:1536px) 600px, 745px"
+                  alt="denis"
                 />
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* SECTION ACTIVITE  */}
+
+      <section class="section section-activite py-32 rounded-b-[80px]">
+        <div class="row">
+          <div class="container wrapper-talk-about pt-12 pb-[68px] rounded-t-[48px] overflow-hidden">
+            <p class="font-extrabold text-sm text-center uppercase text-accent-700 tracking-[0.08em]">
+              Ils parlent de nous
+            </p>
+            <ul class="flex gap-6 mt-7 marquee">
+              {state.newspapers.concat(state.newspapers).map((item, i) => (
+                <li class="flex-shrink-0" key={"newspaper-" + i}>
+                  <a href={item.link} target="_blank">
+                    <img
+                      src={item.src}
+                      alt={item.alt}
+                      width={248}
+                      height={100}
+                    />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div class="row mt-20">
+          <div class="container">
+            <p class="font-extrabold text-sm text-center uppercase text-accent-700 tracking-[0.08em]">
+              POUR BOOSTER TON ACTIVITÉ, TU PEUX :
+            </p>
+            <div class="flex flex-wrap justify-center max-w-[850px] mx-auto gap-4 mt-10">
+              {state.activities.map((activity, i) => (
+                <div key={"activity-" + i} class="group w-full lg:w-[unset]">
+                  <a
+                    class="flex  items-start lg:items-center bg-accent-100 py-6 px-8 rounded-2xl gap-4 transition-[background-color] duration-300 group-hover:bg-purple-brand"
+                    href={activity.link}
+                  >
+                    <img
+                      src={activity.emoji}
+                      alt={activity.alt}
+                      width={32}
+                      height={32}
+                      loading="lazy"
+                    />
+                    <h3 class="italic font-black text-purple-brand text-xl  lg:text-2xl transition-[color] duration-300 group-hover:text-accent-500">
+                      {" "}
+                      {activity.label}{" "}
+                    </h3>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* SECTION DECOLLAGE  */}
+
+      <section class="bg-purple-brand relative overflow-hidden z-[2]">
+        <img
+          class="absolute -top-10 -right-10 blur-[110px]"
+          src="/img/blobs-blur.svg"
+          loading="lazy"
+          alt="blob blurs"
+          width={666}
+          height={557}
+        />
+        <div class="section section-decollage pt-40 pb-40 overflow-hidden relative z-[4]">
+          <div class="row">
+            <div class="container">
+              <div class="grid grid-cols-12">
+                <div class="col-span-12 lg:col-span-5 lg:col-start-2 order-2 lg:order-1 mt-20 lg:mt-0">
+                  <p class="text-accent-200 font-extrabold text-xs uppercase tracking-[0.12em]">
+                    3, 2, 1 ... DÉCOLLAGE
+                  </p>
+                  <h2 class="mt-4 italic font-black text-3xl lg:text-5xl text-accent-500 leading-[108%]">
+                    À partir de{" "}
+                    <span class="text-white">
+                      49 € <sup>HT/mois </sup>
+                    </span>
+                    et sans engagement
+                  </h2>
+                  <div class="mt-4">
+                    <p class="font-medium text-lg text-accent-200 leading-[156%]">
+                      Indépendant, petite ou moyenne entreprise, découvre ALEO,
+                      l’offre qui va révolutionner la manière dont tu fais ta
+                      pub.
+                    </p>
+                    <p class="mt-2 font-medium text-lg text-accent-200 leading-[156%]">
+                      Des solutions digitales, professionnelles et clés en main
+                      pour booster ton activité.
+                    </p>
+                    <p class="mt-2 font-medium text-lg text-accent-200 leading-[156%]">
+                      Attache ta ceinture !
+                    </p>
+                  </div>
+                  {/* <Belt client:visible /> */}
+                  <div class="relative w-fit-content">
+                    <img
+                      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                      src="/img/belt.png"
+                      alt="belt off"
+                      loading="lazy"
+                      width={137}
+                      height={27}
+                    />
+
+                    <img
+                      class="relative z-[2] belt-on opacity-0"
+                      src="/img/belt-on.png"
+                      alt="belt off"
+                      loading="lazy"
+                      width={169}
+                      height={69}
+                    />
+                  </div>
+                </div>
+                <div class="col-span-12 lg:col-span-6 relative order-1 lg:order-2">
+                  <Image
+                    src="https://res.cloudinary.com/dhnxwmjgn/image/upload/f_auto/v1683789845/visuel-decollage.png"
+                    class="lg:absolute lg:right-[-120px] scale-[1.2] lg:scale-1"
+                    breakpoints={[775, 1550]}
+                    height={553}
+                    width={775}
+                    sizes="(max-width:1536px) 600px, 775px"
+                    alt="Mockup science de la beauté"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row py-[200px] my-20">
+            <div class="container">
+              <div class="italic font-black text-[44px] lg:text-[66px] text-accent-100 text-center relative">
+                + de <span class="ten-k">10 000</span> entreprises accompagnées
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="container">
+              {/* <Animation label="lottie-1" client:visible /> */}
+              <Lotties
+                listLotties={state.lottieVisibility.listLotties}
+                list={state.lottieVisibility.list}
+                title={state.lottieVisibility.title}
+                label={"lottie-1"}
+              />
+            </div>
+          </div>
+
+          <div class="row mt-40">
+            <div class="container">
+              {/* <Animation
+                list-lotties={lotties}
+                invert={true}
+                list={bigBang}
+                title="Crée le big bang dans ta communication"
+                label="lottie-2"
+                client:visible
+              /> */}
             </div>
           </div>
         </div>
@@ -68,7 +332,7 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Aleo in Qwik",
   meta: [
     {
       name: "description",
