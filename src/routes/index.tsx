4616760caefafedfe15ae65c75c401bad0360b6c
othy54 <othman.bensaoula@gmail.com>
2023-05-11 20:51:27 +0200

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -148,6 +148,7 @@ export default component$(() => {
                 <Image
                   src="https://res.cloudinary.com/dhnxwmjgn/image/upload/f_webp,q_60/v1683726098/denis-hero-home.png"
                   class="absolute -bottom-32"
+                  priority={true}
                   breakpoints={[350, 700, 674, 1490]}
                   height={626}
                   width={674}
