1bc46aa949c0c6bc70fb5e62db4866df4040d4ec
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-12 12:24:22 +0200

    update

--- src/root.tsx
+++ src/root.tsx
@@ -24,16 +24,18 @@ export default component$(() => {
    * Dont remove the `<head>` and `<body>` elements.
    */
 
+  const script = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KL52VP3');`;
+
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <QwikPartytown forward={["dataLayer.push"]} />
-        <script
-          async
-          type="text/partytown"
-          src="https://www.googletagmanager.com/gtag/js?id=G-KL52VP3"
-        />
+        <script async type="text/partytown" dangerouslySetInnerHTML={script} />
 
         <link
           rel="preload"
