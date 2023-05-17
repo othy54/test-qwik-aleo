43be2372b1f78eef6afc7c7c9377533039f59f86
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-17 12:36:37 +0200

    update

--- adapters/netlify-edge/vite.config.ts
+++ adapters/netlify-edge/vite.config.ts
@@ -1,6 +1,6 @@
 import { netlifyEdgeAdapter } from '@builder.io/qwik-city/adapters/netlify-edge/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
-import baseConfig from '../../vite.config.mjs';
+import baseConfig from '../../vite.config';
 
 export default extendConfig(baseConfig, () => {
   return {
