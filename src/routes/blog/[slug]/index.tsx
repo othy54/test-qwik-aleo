48f64df5b8905b5e08e61d3899bd80c1c8ecae3a
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2023-05-17 15:36:25 +0200

    update

--- src/routes/blog/[slug]/index.tsx
+++ src/routes/blog/[slug]/index.tsx
@@ -1,43 +1,51 @@
 import {
   component$,
-  useResource$,
-  Resource,
+  // useResource$,
+  // Resource,
   useStylesScoped$,
 } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import type { DocumentHead } from "@builder.io/qwik-city";
 import styles from "./index.css?inline";
 
+// eslint-disable-next-line qwik/loader-location
+export const getContentsPost = routeLoader$(async (requestEvent) => {
+  const res = await fetch(
+    `https://nancomcy54-blogaleoapi.pf3000.wpserveur.net/wp-json/wp/v2/posts?slug=${requestEvent.params.slug}`
+  );
+  const post = await res.json();
+  return post;
+});
+
 export default component$(() => {
+  const signal = getContentsPost();
   useStylesScoped$(styles);
-  const { params } = useLocation();
-  const contents = useResource$<any>(async () => {
-    const res = await fetch(
-      `https://nancomcy54-blogaleoapi.pf3000.wpserveur.net/wp-json/wp/v2/posts?slug=${params.slug}`
-    );
-    const data = res.json();
-    return data;
-  });
+
   return (
     <article class="section py-20">
       <div class="row">
         <div class="container-article">
-          <Resource
-            value={contents}
-            onResolved={(post) => {
-              return (
-                <>
-                  <h1 class="text-[56px] italic leading-[110%] font-extrabold text-primary-500 mb-8">
-                    {post[0].title.rendered}
-                  </h1>
-                  <div>
-                    <p dangerouslySetInnerHTML={post[0].content.rendered}></p>
-                  </div>
-                </>
-              );
-            }}
-          />
+          <h1 class="text-[56px] italic leading-[110%] font-extrabold text-primary-500 mb-8">
+            {signal.value[0].title.rendered}
+          </h1>
+          <div>
+            <p dangerouslySetInnerHTML={signal.value[0].content.rendered}></p>
+          </div>
         </div>
       </div>
     </article>
   );
 });
+
+export const head: DocumentHead = ({ resolveValue }) => {
+  const contents = resolveValue(getContentsPost);
+  return {
+    title: contents[0].yoast_head_json.title,
+    meta: [
+      {
+        name: "description",
+        content: contents[0].yoast_head_json.description,
+      },
+    ],
+  };
+};
